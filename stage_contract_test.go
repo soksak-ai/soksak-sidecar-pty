91ada64087107d7ef8169a2d520d1c4cdd3bc414
max <max@blue.tools>
2026-08-22 17:43:06 +0900

    fix: sort PTY release targets

--- stage_contract_test.go
+++ stage_contract_test.go
@@ -25,8 +25,8 @@ func TestReleaseTargetsContainOnlyVerifiedRuntimePlatforms(t *testing.T) {
 		"aarch64-apple-darwin",
 		"aarch64-unknown-linux-gnu",
 		"x86_64-apple-darwin",
-		"x86_64-unknown-linux-gnu",
 		"x86_64-pc-windows-msvc",
+		"x86_64-unknown-linux-gnu",
 	}
 	if len(targets) != len(want) {
 		t.Fatalf("release targets=%v, want %v", targets, want)
